7d9624ebea570a270e458fa030c3f7dbd5242965
jcfido <jcfid@m2-soft.ru>
2021-03-15 22:17:41 +0500

    добавить меню исправить опечатки добавить кнопку распечатать

--- resources/views/generateConfig.blade.php
+++ resources/views/generateConfig.blade.php
@@ -1,32 +1,15 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS ▢ -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
-    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-    <title>ISchool</title>
-  </head>
-  <body class="p-5">
+@extends('app')
+@section('content')
     <h4>Вычисления</h4>
     <form id="generate" action="{{ route('generate::example') }}" method="post">
       @csrf
     </form>
     <div class="row">
-      <div class="col">
+      <div class="col-12 col-md-6">
         <label for="count" class="form-label">Количество примеров <i><b>примерно 100 на лист</b></i></label>
         <input type="number" class="form-control" name="example_count" id="count" placeholder="10" value=10 form="generate">
       </div>
-      <div class="col">
+      <div class="col-12 col-md-6">
         <label for="max" class="form-label">Максимальное число</label>
         <input type="number" class="form-control" name="example_max" id="max"  value=20 form="generate">
       </div>
@@ -61,11 +44,11 @@
     <hr>
     <h4>Сравнения </h4>
     <div class="row">
-      <div class="col">
+      <div class="col-12 col-md-6">
         <label for="comparison_count" class="form-label">Количество примеров <i><b>примерно 100 на лист</b></i></label>
         <input type="number" class="form-control" name="comparison_count" id="comparison_count" placeholder="10" value=10 form="generate">
       </div>
-      <div class="col">
+      <div class="col-12 col-md-6">
         <label for="comparison_max" class="form-label">Максимальное число</label>
         <input type="number" class="form-control"  name="comparison_max" id="comparison_max"  value=20 form="generate">
       </div>
@@ -106,31 +89,4 @@
     <div class="row p-3">
       <button type="submit" class="btn btn-primary" form="generate">Создать примеры</button>
     </div>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-    -->
-  </body>
-  <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(73223356, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        ecommerce:"dataLayer"
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/73223356" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-</html>
+@endsection
